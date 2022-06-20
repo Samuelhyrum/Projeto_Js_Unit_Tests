43bbfd573c0bc5906634c73c957d1bfe4faa105e
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-20 18:48:15 -0300

    Requisito 2git add .

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,10 +16,23 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
+    expect(numbers([1,2,3,4,5])).toEqual(true);
+
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     // Escreva um teste em que a função recebe [' '] e retorna false
   });
+
+  test('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
+    expect(numbers([1,2,'3',4,5])).toEqual(false);
+  });
+
+  test('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
+    expect(numbers([1,'a',3])).toEqual(false);
+  })
+  
+  test('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
+  expect(numbers([' '])).toEqual(false);
+  })
 });
