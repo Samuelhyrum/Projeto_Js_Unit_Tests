70cf15fb392c90c060d0632f9c2d4a7ebdc045c6
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-21 17:27:05 -0300

    Ajeitando lint

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,13 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) =>({
+const createStudent = (name) => ({
   name,
   feedback: () => 'Eita pessoa boa!',
 });
 
 const estudante = createStudent('Leandrão, o Lobo Solitário');
-
-
-
 module.exports = createStudent;
