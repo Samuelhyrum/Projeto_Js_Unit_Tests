54d11d95b22456dbccb78c17bc2095204ac31ed1
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-21 20:29:45 -0300

    Metade do requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,13 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => ({
+  sum: Math.floor(number1 + number2),
+  mult: Math.floor(number1 * number2),
+  div: Math.floor(number1 / number2),
+  sub: Math.floor(number1 - number2),
+});
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => { };
 
 module.exports = { calculator, arrayGenerator };
